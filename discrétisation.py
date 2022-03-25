6b1f8ddb0a896c505caa65476ee1de6b76a2009c
JeremyBelec <101425479+JeremyBelec@users.noreply.github.com>
2022-03-25 15:53:20 -0400

    Update discrétisation.py

--- discrétisation.py
+++ discrétisation.py
@@ -1,12 +1,11 @@
 def getGradient(var,dx):
   """
-  Calcul d'un gradient de flux
-  f est la matrice de champs
+  Calcul d'un gradient 
+  var est la matrice de la variable sur laquelle on applique le grad
   dx est la taille de cellule
-  f_dx est la matrice de la derivé de f dans la direction x
-  f_dy est la matrice de la derivé de f dans la direction y
+  var_dx est la matrice de la derivé de la variable dans la direction x
+  var_dy est la matrice de la derivé de la variable dans la direction y
   variable indexe de la variable à dériver dans points
-  position est l'indexe de la position
   """
 
   var_dx = ( np.roll(var,-1,axis=0) - np.roll(var,1,axis=0) ) / (2*dx)
@@ -17,11 +16,11 @@ def getGradient(var,dx):
   def extrapo(var,var_dx,var_dy,dx):
     
   var_xl = var - var_dx * dx/2
-  var_xl = np.roll(var_XL,-1,axis=0)
+  var_xl = np.roll(var_xl,-1,axis=0)
   var_xr = var + var_dx * dx/2
   
   var_yl = var - var_dy * dx/2
-  var_yl = np.roll(var_YL,-1,axis=1)
+  var_yl = np.roll(var_yl,-1,axis=1)
   var_yr = var + var_dy * dx/2  
     
 
