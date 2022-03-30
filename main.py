439814763949eee8e4c9fdbdd62adda7a619a407
Rouxsty <101423076+Rouxsty@users.noreply.github.com>
2022-03-29 20:11:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -46,3 +46,14 @@ def main():
         vx_dx, vx_dy = getGradient(vx,dx)
         vy_dx, vy_dy = getGradient(vy,dx)
         pression_dx, pression_dy = getGradient(P,dx)
+        rhoprime = rho - 0.5*dt*(rho_dx*vx+rho*vx_dx+rho_dy*vy+rho*vy_dy)
+        vxprime = vx - 0.5*dt*(vx_dx*vx+vy*vx_dy+rho**(-1)*pression_dx)
+        vxprime = vy - 0.5*dt*(vy_dx*vx+vy*vy_dy+rho**(-1)*pression_dy)
+        pressionprime = P - 0.5*dt*(gammma*P*(vx_dx+vy_dy)+vx*pression_dy+vy*pression_dy)
+        rho_L, rho_R, rho_T, rho_B =extrapo (rhoprime, rho_dx, rho_dy, dx)
+        vx_L, vx_R, vx_T, vx_B =extrapo (vxprime, vx_dx, vx_dy, dx)
+        vy_L, vy_R, vy_T, vy_B =extrapo (vyprime, vy_dx, vy_dy, dx)
+        pression_L, pression_R, pression_T, pression_B =extrapo (pressionprime, pression_dx, pression_dy, dx)
+        
+        
+        
